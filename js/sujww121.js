97784885bfe9c7744943144f71b3daf5532887c3
luludproject <luludproject@gmail.com>
2025-03-17 16:04:32 +0900

    first commit

--- js/sujww121.js
+++ js/sujww121.js
@@ -1,8 +1,8 @@
 var jsonldData = {
   "@context": "https://schema.org",
   "@type": "Organization",
-  url: "https://namdong.jianhomecare.com",
-  logo: "https://namdong.jianhomecare.com/img/변기막힘하수구막힘.png",
+  url: "https://bupyeong.jianhomecare.com",
+  logo: "https://bupyeong.jianhomecare.com/img/변기막힘하수구막힘.png",
 };
 
 var script = document.createElement("script");

aafaf444eb093b7490d5466eb5ae3390dfdb4e6f
luludproject <luludproject@gmail.com>
2025-06-23 16:18:03 +0900

    first commit

--- js/sujww121.js
+++ js/sujww121.js
@@ -1,8 +1,8 @@
 var jsonldData = {
   "@context": "https://schema.org",
   "@type": "Organization",
-  url: "https://busan-jingu.jianhomecare.com",
-  logo: "https://yeongdogu.jianhomecare.com/img/변기막힘하수구막힘.png",
+  url: "https://dongnaegu.jianhomecare.com",
+  logo: "https://dongnaegu.jianhomecare.com/img/변기막힘하수구막힘.png",
 };
 
 var script = document.createElement("script");
